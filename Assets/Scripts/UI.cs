106ecb81eae5378d53999bdb8a77c0cc987df60d
abcd134 <jbala972@mtroyal.ca>
2015-11-04 10:25:25 -0700

    HP
    Added HP UI

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI : MonoBehaviour
+{
+    public Sprite[] heartSprite;
+    public Image HeartUI;
+    public int HP;
+
+   void Update ()
+    {
+        HeartUI.sprite = heartSprite[HP];
+
+    }
+}
+
