ec4e945236ab8eccf6c9a5212677b3daf093f96a
abcd134 <jbala972@mtroyal.ca>
2015-11-07 01:04:05 -0700

    LevelAndScore
    Added Level and Score Function

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -6,12 +6,18 @@ public class UI : MonoBehaviour
 {
     public Sprite[] heartSprite;
     public Image HeartUI;
+    public Text PointsUI;
+    public Text LevelUI;
     public int HP;
+    public int Points;
+    public int Level;
+    private string levelPrefix = "Level: ";
 
    void Update ()
     {
         HeartUI.sprite = heartSprite[HP];
-
+        PointsUI.text = Points.ToString();
+        LevelUI.text = levelPrefix + Level.ToString();
     }
 }
 
