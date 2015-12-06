232a4a4f5bf111f5aee2fda9e3ea34d4f1ef4f12
abcd134 <jbala972@mtroyal.ca>
2015-12-06 15:42:33 -0700

    MinionFlip

---
+++ Assets/DetectGround.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectGround : MonoBehaviour {
+    GameObject edgeDetector;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Floor")
+        {
+            Debug.Log("MinionNotOnFloor");
+            Vector3 asd = gameObject.transform.position;
+            gameObject.transform.position.Set(-1 * asd.x, asd.y, asd.z);
+        }
+    }
+}
